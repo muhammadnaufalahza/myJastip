1f6998dec5d5119c9db120540472809c90a962bc
muhammadnaufalahza <muhammadnaufalahza@student.telkomuniversity.ac.id>
2026-05-23 22:25:33 +0700

    ganti dari d2 ke postgreSQL

--- src/main/java/myjastip/DatabaseUtil.java
+++ src/main/java/myjastip/DatabaseUtil.java
@@ -3,13 +3,37 @@ package myjastip;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.Scanner;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:h2:./myjastip;AUTO_SERVER=TRUE";
-    private static final String USER = "root";
-    private static final String PASS = "password";
+	
+//    private static final String URL = "jdbc:h2:./myjastip;AUTO_SERVER=TRUE";
+    private static final String URL = "jdbc:postgresql://localhost:5432/MYJASTIP";
+    private static final String USER = "postgres";
+//    private static final String PASS = "";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+    	Scanner sc = new Scanner(System.in);
+    	System.out.printf("Masukkan Password PostgreSQL: ");
+        return DriverManager.getConnection(URL, USER, sc.nextLine());
+    }
+    
+    public static void testDB() {
+        
+    	try (Connection conn = getConnection()) {
+    		
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT version()");
+
+            if (rs.next()) {
+                System.out.println("DB Version: " + rs.getString(1));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    	
     }
 }
--- src/main/java/myjastip/MyJastip.java
+++ src/main/java/myjastip/MyJastip.java
@@ -1,8 +1,11 @@
 package myjastip;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MyJastip extends Application {
@@ -11,7 +14,7 @@ public class MyJastip extends Application {
 	public void start(Stage stage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(createContent(),400,400);
 			stage.setScene(scene);
 			stage.show();
 		} catch(Exception e) {
@@ -19,10 +22,16 @@ public class MyJastip extends Application {
 		}
 	}
 	
-	public static void main(String[] args) {				
-		System.out.println("hello world");
+	private Parent createContent() {
+        return new StackPane(new Text("Sistem Jastip"));
+    }
+	
+	public static void main(String[] args) throws Exception {				
+//		DatabaseUtil.testDB();
+//		Perlu PostgreSQL
 		
 		Application.launch(MyJastip.class, args);
+		
 		
 	}
 
