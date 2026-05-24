c561b8d66c560882c971ac443b3257c6e859c380
Naufal Ahza <ahzanaufal86@gmail.com>
2026-05-25 00:32:21 +0700

    testt4

--- src/main/java/myjastip/MyJastip.java
+++ src/main/java/myjastip/MyJastip.java
@@ -26,7 +26,7 @@ public class MyJastip extends Application {
     }
 	
 	public static void main(String[] args) {	
-		System.out.println("Hello World");
+		System.out.println("--Hello World--");
 
                 
 //		DatabaseUtil.testDB();
