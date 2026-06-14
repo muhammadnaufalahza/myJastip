3ff6f69f8e906d8ffb9f40780cfacb42e5a9565f
bagas55555505-ops <bagas55555505@gmail.com>
2026-06-14 13:29:27 +0700

    rapikan code

--- src/main/java/myjastip/payment/Escrow.java
+++ src/main/java/myjastip/payment/Escrow.java
@@ -1,20 +1,24 @@
 package myjastip.payment;
 
-public class Escrow extends Payment {
-    private String transactionId;
-    private double amount;
-    private String status;
+public class Escrow{
+private String transactionId;
+private double amount;
+private String status;
 
-    public void holdFunds(){
-        this.status = "Tahan";
-        System.out.println("Sistem Escrow: Dana sebesar " + this.amount + " ditahan untuk transaksi ID: " + this.transactionId);
-    }
+public void holdFunds(){
+      this.status = "Tahan";
+      System.out.println("Sistem Escrow: Dana sebesar " + this.amount + " ditahan untuk transaksi ID: " + this.transactionId);
+}
 
-    public void releaseFunds(){
-        this.status = "Lepas";
-        System.out.println("Sistem Escrow: Dana sebesar " + this.amount + " di lepas untuk transaksi ID: " + this.transactionId);
-    }
-    public void refundFunds(){
+
+
+public void releaseFunds(){
+       this.status = "Lepas";
+       System.out.println("Sistem Escrow: Dana sebesar " + this.amount + " di lepas untuk transaksi ID: " + this.transactionId);
+}
+
+
+public void refundFunds(){
         this.status = "Refund";
         System.out.println("Sistem Escrow: Transaksi batal. Dana dikembalikan ke Customer.");
     }
